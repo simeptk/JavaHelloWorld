bcfe6ea015760cf6b5a5aba000bc27e5e09b4c79
pkumara2 <pkumara2@ford.com>
2019-12-07 16:58:12 +0530

    Updated HelloWorld java program

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,16 @@
 // Your First Program
-class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!"); 
-        System.out.println("Designed By Kumaran P T . . .!");     }
+import java.util.Date;
+
+class HelloWorld 
+{
+	public static void main(String[] args)
+	{
+        	System.out.println("Hello, World!"); 
+        	System.out.println("Designed By Kumaran P T . . .!");
+		// Instantiate a Date object
+		Date date = new Date();
+
+		// display time and date using toString()
+		System.out.println(date.toString());
+	}
 }
