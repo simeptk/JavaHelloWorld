c6da53df67c87968c1ec67be0d40c4e347736786
pkumara2 <pkumara2@ford.com>
2019-12-07 17:04:39 +0530

    Removed Unwanted comments in Java Program file

--- HelloWorld.java
+++ HelloWorld.java
@@ -7,10 +7,7 @@ class HelloWorld
 	{
         	System.out.println("Hello, World!"); 
         	System.out.println("Designed By Kumaran P T . . .!");
-		// Instantiate a Date object
 		Date date = new Date();
-
-		// display time and date using toString()
 		System.out.println(date.toString());
 	}
 }
