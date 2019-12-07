0241986767fdacc1c5e39504699056edcc181b4c
pkumara2 <pkumara2@ford.com>
2019-12-07 17:13:52 +0530

    Updated the code in java files

--- HelloWorld.java
+++ HelloWorld.java
@@ -6,8 +6,8 @@ class HelloWorld
 	public static void main(String[] args)
 	{
         	System.out.println("Hello, World!"); 
-        	System.out.println("Designed By Kumaran P T . . .!");
 		Date date = new Date();
 		System.out.println(date.toString());
+        	System.out.println("Designed By Kumaran P T . . .!");
 	}
 }
